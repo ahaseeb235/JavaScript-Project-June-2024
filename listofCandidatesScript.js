a66e14de50f974260ca88ca10add8b2671b125e7
ahaseeb235 <ahaseeb235@gmail.com>
2024-06-24 20:37:11 +0100

    Committing on Sunday

---
+++ listofCandidatesScript.js
@@ -0,0 +1,116 @@
+// Fetch and initialize data
+let candidates = [];
+fetch('finalListOfCandidates2024.json')
+    .then(response => response.json())
+    .then(data => {
+        candidates = data;
+        populateAlphabetFilter();
+        populateRegionFilter();
+        populatePoliticalPartyFilter();
+        displayCandidates(candidates);
+    });
+
+// Populate alphabet filter
+function populateAlphabetFilter() {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetFilter = document.getElementById('alphabetFilter');
+    
+    for (let char of alphabet) {
+        const button = document.createElement('button');
+        button.textContent = char;
+        button.onclick = () => filterByAlphabet(char);
+        alphabetFilter.appendChild(button);
+    }
+}
+
+// Populate region filter
+function populateRegionFilter() {
+    const regions = [...new Set(candidates.map(candidate => candidate.constituency))];
+    const regionFilter = document.getElementById('regionFilter');
+    
+    regions.forEach(region => {
+        const option = document.createElement('option');
+        option.value = region;
+        option.textContent = region;
+        regionFilter.appendChild(option);
+    });
+}
+
+// populate political party filter
+function populatePoliticalPartyFilter() {
+    const politicalPartys = [...new Set(candidates.map(candidate => candidate.politicalParty))];
+    
+    politicalPartys.forEach(politicalParty => {
+        const option = document.createElement('option');
+        option.value = politicalParty;
+        option.textContent = politicalParty;
+        politicalPartyFilter.appendChild(option);
+    });
+}
+
+
+
+
+// Display candidates in table
+function displayCandidates(candidatesToDisplay) {
+    const tbody = document.getElementById('candidateTable').getElementsByTagName('tbody')[0];
+    tbody.innerHTML = '';
+
+    candidatesToDisplay.forEach(candidate => {
+        const row = document.createElement('tr');
+        
+       
+        for (let key in candidate) {
+            const cell = document.createElement('td');
+            cell.textContent = candidate[key];
+            row.appendChild(cell);
+        }
+
+    // Adding highlight class if the candidate is elected
+        if (candidate.finalVerdict === 'Elected') {
+            row.style.backgroundColor = 'lightgreen';
+        }
+
+        tbody.appendChild(row);
+    });
+}
+
+// Filter by alphabet
+function filterByAlphabet(char) {
+    let filteredCandidates = candidates;
+
+    if (char !== 'All') {
+        filteredCandidates = candidates.filter(candidate => candidate.fullName.startsWith(char));
+    }
+
+    applyFilters(filteredCandidates);
+}
+
+// Apply gender, region, and final verdict filters
+function applyFilters(filteredCandidates = candidates) {
+    const genderFilter = document.getElementById('genderFilter').value;
+    const regionFilter = document.getElementById('regionFilter').value;
+    const finalVerdictFilter = document.getElementById('finalVerdictFilter').value;
+    const politicalPartyFilter = document.getElementById('politicalPartyFilter').value;
+
+    if (genderFilter) {
+        filteredCandidates = filteredCandidates.filter(candidate => candidate.Gender === genderFilter);
+    }
+
+    if (regionFilter) {
+        filteredCandidates = filteredCandidates.filter(candidate => candidate.constituency === regionFilter);
+    }
+
+    if (finalVerdictFilter) {
+        filteredCandidates = filteredCandidates.filter(candidate => candidate.finalVerdict === finalVerdictFilter);
+    }
+
+    if (politicalPartyFilter) {
+        filteredCandidates = filteredCandidates.filter(candidate => candidate.politcalParty === politicalPartyFilter);
+    }
+
+    displayCandidates(filteredCandidates);
+}
+
+
+
